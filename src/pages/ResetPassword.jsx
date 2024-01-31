1915d79fdd6b932e3496417251d7d7580494f042
Akhila Vijayan <akhilavijayan964@gmail.com>
2024-01-31 14:38:47 +0530

    initial commit

---
+++ src/pages/ResetPassword.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ResetPassword = () => {
+    return (
+        <div>
+            <h1>ResetPAssword</h1>
+        </div>
+    )
+}
+
+export default ResetPassword;
